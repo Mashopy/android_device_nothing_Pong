a2e6db4db46dd7bd169d1097c8ad8cfe9426453f
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-21 01:54:58 +0300

    Pong: Avoid sensors multihal name conflicts
    Change-Id: If32d1c9a667d3ce71a1efaa9d358770f925f0f27
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -394,7 +394,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors@2.1-service.nt.pong-multihal \
     libsensorndkbridge \
     sensors.nt.pong
 
