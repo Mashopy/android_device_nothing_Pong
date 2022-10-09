5640f947de68c3e20690e63e555751d9322116e9
Kujou Yuko <i@779.moe>
2022-10-09 13:01:53 +0000

    Pong: Add OSS fingerprint 2.1 HAL implementation
    Signed-off-by: Kujou Yuko <i@779.moe>
    Change-Id: Ic8d5b23ebe532230fbba3ac2e576a27a80ae27c7

--- device.mk
+++ device.mk
@@ -183,6 +183,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
 # Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.nt.pong
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
