4e623ae205e308fbc2490063ae38704c14ff85da
chandu078 <chandudyavanapelli03@gmail.com>
2023-10-02 12:48:31 +0000

    Pong: Import cleaned taro RROs from NOS T2.0-230906-1933
    Change-Id: I9807cfc2727aad7f3555a3b709f732e2acad8e96
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -308,6 +308,11 @@ PRODUCT_PACKAGES += \
     WifiResMainlineCommon \
     WifiResCommon
 
+# Taro overlays
+PRODUCT_PACKAGES += \
+    FrameworksResTarget \
+    WifiResTarget
+
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
