560430bd0021b03879fa4c971cb451ad334cacb2
someone5678 <someone5678@users.noreply.github.com>
2024-06-08 22:59:05 +0900

    Pong: Use QCOM USB gadget HAL
    * Newer SoC's like taro or kalama use gadget HAL
    
    Change-Id: Ib3383bc1173db22c539b2073d72419c42fcf92cc
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -249,8 +249,6 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.recovery.rc \
     init.qcom.sh \
-    init.qcom.usb.rc \
-    init.qcom.usb.sh \
     init.target.rc \
     ueventd.nt.rc \
     ueventd.qcom.rc
@@ -473,7 +471,11 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb@1.3-service-qti \
+    android.hardware.usb.gadget@1.2-service-qti \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
+    usb_compositions.conf
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
