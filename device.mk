0866f6591d69bd085b73c3b8c71a624516f4a7c6
chandu078 <chandudyavanapelli03@gmail.com>
2024-11-12 19:13:40 +0530

    Pong: Move to external kernel devicetrees and modules
    Change-Id: Ib5ce6d0d04796fab27d4b7efbcdc5993a29853f9
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -320,7 +320,9 @@ PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    kernel/nothing/sm8475 \
+    kernel/nothing/sm8475-modules
 
 # Telephony
 PRODUCT_PACKAGES += \
