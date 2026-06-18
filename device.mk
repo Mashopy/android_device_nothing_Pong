5195abc2d33d8d74b121647253fb5b2787226775
Mashopy <eliasgheeraert@gmail.com>
2026-06-18 18:48:02 +0200

    Pong: Set correct screen height
    Change-Id: Id1418e95ee3a40c660839971909fb48ff7016360

--- device.mk
+++ device.mk
@@ -95,7 +95,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2376
+TARGET_SCREEN_HEIGHT := 2412
 TARGET_SCREEN_WIDTH := 1080
 
 # Boot control
