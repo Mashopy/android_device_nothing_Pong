1fa79fbab710d0493ff2ec9681218315c06f87c9
BrainKub <fyhtjnt@gmail.com>
2026-08-18 01:38:46 +0300

    Pong: Move to Nothing fingerprint AIDL
    Change-Id: Ib34b747c4b9900ab53e93e4854afe795fdbc080b

--- device.mk
+++ device.mk
@@ -134,7 +134,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.nothing \
+    android.hardware.biometrics.fingerprint-service.nothing \
     libudfpshandler
 
 PRODUCT_COPY_FILES += \
